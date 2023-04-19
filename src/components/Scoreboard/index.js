c71e4a8e48e5d8cde8ff8d7fa67fe4e696d00ba5
Alex Wade <alexjohnwade@gmail.com>
2023-04-19 11:24:25 +1000

    got score displaying instead of input

--- src/components/Scoreboard/index.js
+++ src/components/Scoreboard/index.js
@@ -2,35 +2,44 @@ import { useState } from "react"
 import { useParams } from "react-router-dom"
 
 function Scoreboard () {
-  const [submitScore, setSubmitScore] = useState(false)
-  const [name, setName] = useState('')
+  // const [submitScore, setSubmitScore] = useState(false)
+  // const [name, setName] = useState('')
   let {score} = useParams()
 
-  const handleName = (event) => {
-    setName(event.target.value)
-  }
+  // const handleName = (event) => {
+  //   setName(event.target.value)
+  // }
 
-  const handleSubmit = () => {
-    setSubmitScore(true)
-  }
+  // const handleSubmit = () => {
+  //   setSubmitScore(true)
+  // }
 
 
 
-  if (submitScore) {
+//   if (submitScore) {
 
-    return (
-      <div className="container">
-        <p>{name}:{score}</p>
+//     return (
+//       <div className="container">
+//         <p>{name}:{score}</p>
+//       </div>
+//     )
+//   } else {
+//     return (
+//       <div className="container">
+//         <input onChange={handleName}></input>
+//         <button onClick={handleSubmit}>submit</button>
+//       </div>
+//       )
+//     }
+
+  return (
+    <div className="container">
+      <div className="scorebox">
+        <h3>You got {score}/10!</h3>
       </div>
-    )
-  } else {
-    return (
-      <div className="container">
-        <input onChange={handleName}></input>
-        <button onClick={handleSubmit}>submit</button>
-      </div>
-      )
-    }
+    </div>
+  )
+
 }
 
 export default Scoreboard
